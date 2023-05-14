aa3f84af413a43d3c49478deab32d9400814db18
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-05-14 16:42:20 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,14 @@
 import user from '../data/user.json';
-
+import { Profile } from '../components/Profile.jsx';
 
 export const App = () => {
   return (
-    <div class="profile">
-      <div class="description">
-        <img src={user.avatar} alt="User avatar" class="avatar" />
-        <p class="name">{user.username}</p>
-        <p class="tag">{user.tag}</p>
-        <p class="location">{user.location}</p>
-      </div>
-
-      <ul class="stats">
-        <li>
-          <span class="label">Followers</span>
-          <span class="quantity">: {user.stats.followers}</span>
-        </li>
-        <li>
-          <span class="label">Views</span>
-          <span class="quantity">: {user.stats.views}</span>
-        </li>
-        <li>
-          <span class="label">Likes</span>
-          <span class="quantity">: {user.stats.likes}</span>
-        </li>
-      </ul>
-    </div>
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
   );
 };
