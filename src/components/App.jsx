cb05bcb5415dd32b51117ff9e81379a93cbb6d71
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-05-17 19:18:30 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
     </>
   );
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,26 +1,13 @@
-export const Statistics = () => {
+export const Statistics = ({ stats }) => {
   return (
     <section className="statistics">
       <h2 className="title">Upload stats</h2>
-
-      <ul className="stat-list">
-        <li className="item">
-          <span className="label">.docx</span>
-          <span className="percentage">4%</span>
+      {stats.map(stat => (
+        <li className="item" key={stat.id}>
+          <span className="label">{stat.label}</span>
+          <span className="percentage">{stat.percentage}%</span>
         </li>
-        <li className="item">
-          <span className="label">.mp3</span>
-          <span className="percentage">14%</span>
-        </li>
-        <li className="item">
-          <span className="label">.pdf</span>
-          <span className="percentage">41%</span>
-        </li>
-        <li className="item">
-          <span className="label">.mp4</span>
-          <span className="percentage">12%</span>
-        </li>
-      </ul>
+      ))}
     </section>
   );
 };
