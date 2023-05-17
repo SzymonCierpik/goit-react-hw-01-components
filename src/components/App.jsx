400f0580ab335af171d650a63be1a5738261a9c0
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-05-17 12:58:06 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import Profile from './Profile/Profile';
+import { Profile } from './Profile/Profile';
 import user from './Profile/user.json';
-import Statistics from './Statistics/Statistics';
-import data from './Statistics/data.json';
-import FriendsList from './FriendsList/FriendsList';
-import friends from './FriendsList/friends.json';
-import TransactionHistory from './TransactionHistory/TransactionHistory';
-import transaction from './TransactionHistory/transactions.json';
+
 
 const App = () => {
   return (
@@ -18,9 +13,6 @@ const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      <Statistics title="Upload stats" stats={data} />
-      <FriendsList friends={friends} />
-      <TransactionHistory items={transaction} />
     </>
   );
 };
