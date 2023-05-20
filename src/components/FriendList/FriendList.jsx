89151e51c8df654dd98a6da421849555bb7af223
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-05-20 16:52:38 +0200

    css

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,3 +1,4 @@
+import css from './FriendList.module.css';
 export const FriendList = ({ friends }) => {
   return (
     <ul>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,25 +1,27 @@
+import css from './Profile.module.css';
+
 export const Profile = (props) => {
   return (
-    <div className="profile">
-      <div className="description">
-        <img src={props.avatar} alt="User avatar" className="avatar" />
-        <p className="name">{props.username}</p>
-        <p className="tag">{props.tag}</p>
-        <p className="location">{props.location}</p>
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={props.avatar} alt="User avatar" className={css.avatar} />
+        <p className={css.name}>{props.username}</p>
+        <p className={css.tag}>@{props.tag}</p>
+        <p className={css.location}>{props.location}</p>
       </div>
 
-      <ul className="stats">
+      <ul className={css.stats}>
         <li>
-          <span className="label">Followers</span>
-          <span className="quantity">: {props.stats.followers}</span>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{props.stats.followers}</span>
         </li>
         <li>
-          <span className="label">Views</span>
-          <span className="quantity">: {props.stats.views}</span>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{props.stats.views}</span>
         </li>
         <li>
-          <span className="label">Likes</span>
-          <span className="quantity">: {props.stats.likes}</span>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{props.stats.likes}</span>
         </li>
       </ul>
     </div>
